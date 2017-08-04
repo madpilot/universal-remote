db7c58757dc3b8ac29a706c8cb38987b042c041c
Myles Eftos <myles@madpilot.com.au>
2017-08-04 22:41:03 +1000

    Started to add argument parsing. needs refactoring

--- software/lib/cec/mapping/parser.ex
+++ software/lib/cec/mapping/parser.ex
@@ -1,5 +1,5 @@
 defmodule CEC.Mapping.Parser do
-  alias CEC.Mapping.{Source, Destination, OpCodes}
+  alias CEC.Mapping.{Source, Destination, OpCodes, Vendor}
   use Bitwise, only_operators: true
 
   def map_devices(devices) do
@@ -15,10 +15,42 @@ defmodule CEC.Mapping.Parser do
     OpCodes.from_code(opcode |> Integer.parse(16) |> elem(0))
   end
 
+  def arguments_to_string(arguments) do
+    arguments
+    |> Enum.map(fn(int) -> int |> Integer.parse(16) |> elem(0) end)
+    |> to_string
+  end
+
+  def arguments_to_address(arguments) do
+    arguments
+    |> Enum.join(".")
+  end
+
+  def arguments_to_integer(arguments) do
+    arguments
+    |> Enum.join("")
+    |> Integer.parse(16)
+    |> elem(0)
+  end
+
+  def arguments_to_vendor(arguments) do
+    arguments_to_integer(arguments)
+    |> Vendor.from_code
+  end
+
+  def map_arguments(opcode, arguments) do
+    case opcode |> map_opcode do
+      :set_osd_name -> %{value: arguments_to_string(arguments)}
+      :device_vendor_id -> %{vendor: arguments_to_vendor(arguments)}
+
+      _ -> %{arguments: arguments}
+    end
+  end
+
   def map_elements(%{devices: devices, opcode: opcode, arguments: arguments}) do
     %{devices: devices, opcode: opcode}
     |> map_elements()
-    |> Map.merge(%{arguments: arguments})
+    |> Map.merge(map_arguments(opcode, arguments))
   end
 
   def map_elements(%{devices: devices, opcode: opcode}) do
@@ -33,14 +65,15 @@ defmodule CEC.Mapping.Parser do
 
   def process(codes) do
     case codes do
+      [devices | []] -> map_elements(%{devices: devices}) |> Map.merge(%{command: :polling})
+      [devices | [opcode | []]] -> map_elements(%{devices: devices, opcode: opcode})
       [devices | [opcode | arguments]] -> map_elements(%{devices: devices, opcode: opcode, arguments: arguments})
-      [devices | [opcode]] -> map_elements(%{devices: devices, opcode: opcode})
-      [devices] -> map_elements(%{devices: devices})
     end
   end
 
   def from_code(code) do
     code
+    |> Apex.ap
     |> String.trim
     |> String.split(":")
     |> process
