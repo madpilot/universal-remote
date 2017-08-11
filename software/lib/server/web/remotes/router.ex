d6cca4027dd858effb5105b5435e8a335230c59f
Myles Eftos <myles@madpilot.com.au>
2017-08-11 22:36:27 +1000

    Refactoring web api

--- software/lib/server/web/remotes/router.ex
+++ software/lib/server/web/remotes/router.ex
@@ -7,86 +7,84 @@ defmodule Server.Web.Remotes.Router do
                      json_decoder: Poison
   plug :dispatch
 
-  def return_json(conn, json) do
+  def send_json(conn, status, obj) do
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(200, json  |> Poison.encode!)
+    |> send_resp(status, obj |> Poison.encode!)
+  end
+
+  def bus_module(bus) do
+    case bus do
+      "cec" -> {:ok, CEC.Remote}
+      "lirc" -> {:ok, LIRC.Remote}
+      _ -> {:error, :not_found}
+    end
   end
 
   get "/" do
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(200, [:cec, :lirc] |> Poison.encode!)
+    |> send_json(200, [:cec, :lirc])
   end
 
   get "/:bus" do
-    devices = case bus do
-      "cec" -> CEC.Remote.devices
-      "lirc" -> LIRC.Remote.devices
-      _ -> :not_found
-    end
-
-    case devices do
-      :not_found -> send_resp(conn, 404, "Not found")
-      {:ok, list} -> return_json(conn, list)
+    with {:ok, module} <- bus_module(bus),
+         {:ok, list} <- module.devices
+    do
+      send_json(conn, 200, list)
+    else
+      {:error, :not_found} -> send_resp(conn, 404, "Not found")
+      _ ->  send_resp(conn, 500, "Error")
     end
   end
 
   get "/:bus/:device" do
-    commands = case bus do
-      "cec" -> CEC.Remote.commands(device |> String.to_atom)
-      "lirc" -> LIRC.Remote.commands(device |> String.to_atom)
-      _ -> :not_found
-    end
-
-    case commands do
-      :not_found -> send_resp(conn, 404, "Not found")
-      {:ok, list} -> return_json(conn, list)
+    with {:ok, module} <- bus_module(bus),
+         {:ok, list} <- module.commands(device |> String.to_atom)
+    do
+      send_json(conn, 200, list)
+    else
+      {:error, :not_found} -> send_resp(conn, 404, "Not found")
+      _ ->  send_resp(conn, 500, "Error")
     end
   end
 
   put "/:bus/:device/send" do
     %{key: key} = conn.body_params
 
-    commands = case bus do
-      "cec" -> CEC.Remote.send_once(device |> String.to_atom, key |> String.to_atom)
-      "lirc" -> LIRC.Remote.send_once(device |> String.to_atom, key |> String.to_atom)
-      _ -> :not_found
-    end
-
-    case commands do
-      :not_found -> send_resp(conn, 404, "Not found")
-      :ok -> send_resp(conn, 200, "Ok")
+    with {:ok, module} <- bus_module(bus),
+         {:ok} <- module.send_once(device |> String.to_atom, key |> String.to_atom)
+    do
+      send_resp(conn, 200, "OK")
+    else
+      {:error, :not_found} -> send_resp(conn, 404, "Not found")
+      _ ->  send_resp(conn, 500, "Error")
     end
   end
 
   put "/:bus/:device/start" do
     %{key: key} = conn.body_params
 
-    commands = case bus do
-      "cec" -> CEC.Remote.start_send(device |> String.to_atom, key |> String.to_atom)
-      "lirc" -> LIRC.Remote.start_send(device |> String.to_atom, key |> String.to_atom)
-      _ -> :not_found
-    end
-
-    case commands do
-      :not_found -> send_resp(conn, 404, "Not found")
-      :ok -> send_resp(conn, 200, "Ok")
+    with {:ok, module} <- bus_module(bus),
+         {:ok} <- module.start_send(device |> String.to_atom, key |> String.to_atom)
+    do
+      send_resp(conn, 200, "OK")
+    else
+      {:error, :not_found} -> send_resp(conn, 404, "Not found")
+      _ ->  send_resp(conn, 500, "Error")
     end
   end
 
   put "/:bus/:device/stop" do
     %{key: key} = conn.body_params
 
-    commands = case bus do
-      "cec" -> CEC.Remote.stop_send(device |> String.to_atom, key |> String.to_atom)
-      "lirc" -> LIRC.Remote.stop_send(device |> String.to_atom, key |> String.to_atom)
-      _ -> :not_found
-    end
-
-    case commands do
-      :not_found -> send_resp(conn, 404, "Not found")
-      :ok -> send_resp(conn, 200, "Ok")
+    with {:ok, module} <- bus_module(bus),
+         {:ok} <- module.stop_send(device |> String.to_atom, key |> String.to_atom)
+    do
+      send_resp(conn, 200, "OK")
+    else
+      {:error, :not_found} -> send_resp(conn, 404, "Not found")
+      _ ->  send_resp(conn, 500, "Error")
     end
   end
 end
