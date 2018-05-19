cb187a47537db5df1cf30cba4f4074fd2e24b8e1
Myles Eftos <myles@madpilot.com.au>
2018-05-19 22:30:42 +1000

    Testing event listener filter

--- software/spec/lib/devices/one_shot_listener_spec.exs
+++ software/spec/lib/devices/one_shot_listener_spec.exs
@@ -1,17 +1,16 @@
-defmodule ListenerSpec do
+defmodule OneShotListenerSpec do
   use ESpec, async: false
 
   describe "listen_for" do
     let :event, do: %{bus: :cec, command: :cec_version, destination: :audio_system, source: :tuner_2, version: "1.1"}
-
-    before do
-      Devices.Listener.handle_events([event()], nil, %{filter: filter(), caller: self()})
-    end
+    let :state, do: %{filter: filter(), caller: self()}
+    subject do: Devices.OneShotListener.handle_events([event()], nil, state())
 
     describe "complete mismatch" do
       let :filter, do: %{bus: :cec, command: :cec_version, destination: :audio_system, source: :tuner_2, version: "1.2"}
 
       it "sends no message" do
+        expect subject() |> to(eq({:noreply, [], state()}))
         refute_received {:matched, %{bus: :cec, command: :cec_version, destination: :audio_system, source: :tuner_2, version: "1.1"}}
       end
     end
@@ -20,6 +19,7 @@ defmodule ListenerSpec do
       let :filter, do: %{bus: :lirc}
 
       it "sends no message" do
+        expect subject() |> to(eq({:noreply, [], state()}))
         refute_received {:matched, %{bus: :cec, command: :cec_version, destination: :audio_system, source: :tuner_2, version: "1.1"}}
       end
     end
@@ -28,6 +28,7 @@ defmodule ListenerSpec do
       let :filter, do: %{bus: :cec, command: :cec_version}
 
       it "sends the event" do
+        expect subject() |> to(eq({:stop, :normal, state()}))
         assert_received {:matched, %{bus: :cec, command: :cec_version, destination: :audio_system, source: :tuner_2, version: "1.1"}}
       end
     end
@@ -36,6 +37,7 @@ defmodule ListenerSpec do
       let :filter, do: %{bus: :cec, command: :cec_version, destination: :audio_system, source: :tuner_2, version: "1.1"}
 
       it "sends the event" do
+        expect subject() |> to(eq({:stop, :normal, state()}))
         assert_received {:matched, %{bus: :cec, command: :cec_version, destination: :audio_system, source: :tuner_2, version: "1.1"}}
       end
     end
