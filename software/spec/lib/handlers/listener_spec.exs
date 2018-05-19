ba8504e88b2a7515dd67d02bd174ba3cd31d9dda
Myles Eftos <myles@madpilot.com.au>
2018-05-19 11:07:10 +1000

    Adjusting the Listener test

--- software/spec/lib/handlers/listener_spec.exs
+++ software/spec/lib/handlers/listener_spec.exs
@@ -1,27 +1,27 @@
 defmodule ListenerSpec do
   use ESpec
 
-  describe "wait_for" do
-    let :filter, do: %{bus: :cec, command: :cec_version}
-    let :timeout, do: 1000
-
-    let :subject, do: (Listener.wait_for(filter(), timeout()) do
+  describe "listen_for" do
+    before do
+      {:ok, pid} = Devices.Listener.listen_for(filter(), self())
       CEC.SystemAudioControl.give_audio_status(:tv, :audio_system)
       CEC.SystemInformation.cec_version(:tuner_2, :audio_system, "1.1")
-    end)
+      Process.exit(pid, :normal)
+    end
 
     describe "timeout" do
       let :filter, do: %{bus: :lirc}
-      let :timeout, do: 10
 
-      it "return an error" do
-        expect(subject()) |> to(eq({:timeout, "Status request timed out"}))
+      it "sends an error" do
+        refute_receive {:matched, %{bus: :cec, command: :cec_version, destination: :audio_system, source: :tuner_2, version: "1.1"}}
       end
     end
 
     describe "success" do
-      it "returns the event" do
-        expect(subject()) |> to(eq({:ok, %{bus: :cec, command: :cec_version, destination: :audio_system, source: :tuner_2, version: "1.1"}}))
+      let :filter, do: %{bus: :cec, command: :cec_version}
+
+      it "sends the event" do
+        assert_receive {:matched, %{bus: :cec, command: :cec_version, destination: :audio_system, source: :tuner_2, version: "1.1"}}
       end
     end
   end
